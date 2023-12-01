6d6e7f17ecd6436a6838a314c2531102c3e29781
lohanamaria <116275294+lohanamaria@users.noreply.github.com>
2023-12-01 20:40:15 +0000

    Usando UseEffect para pegar os dados de registro e renderizar os videos na página Home com apenas o nome e link da localstorage

--- PTAC-projeto-final-main/src/Home.jsx
+++ PTAC-projeto-final-main/src/Home.jsx
@@ -1,3 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+
+
 export default function Home(){
 return(
 <div class="navbar" id="navbar">
@@ -5,4 +9,37 @@ return(
   <a href="/destaque" class="nav-btn"> Em destaque</a>
   <a href="/registro" class="nav-btn">Novo vídeo</a>
 </div>
+
+const VideosRegistrados = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const storedVideos = JSON.parse(localStorage.getItem('videos')) || [];
+    setVideos(storedVideos);
+  }, []);
+
+  return (
+    <div>
+      {videos.map((video, index) => {
+        return (
+          <div key={index}>
+            <h3>{video.nome}</h3>
+            <iframe
+              width="560"
+              height="315"
+              src={'https://www.youtube.com/embed/' + video.url.slice(17)}
+              title={video.nome}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Home;
+
 )};
